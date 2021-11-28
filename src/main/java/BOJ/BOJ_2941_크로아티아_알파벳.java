3d7d2a0779dcea602383d5c82b22089750e23955
jungdahee <jungdahee010@naver.com>
2021-11-28 15:44:27 +0900

    Initial commit

---
+++ src/main/java/BOJ/BOJ_2941_크로아티아_알파벳.java
@@ -0,0 +1,18 @@
+package BOJ;
+
+import java.io.*;
+
+public class BOJ_2941_크로아티아_알파벳 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        String words[] = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+
+        for(int i = 0; i < words.length; i++){
+            input = input.replaceAll(words[i], ".");
+        }
+
+        System.out.println(input.length());
+    }
+}
